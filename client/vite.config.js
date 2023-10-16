c9f3535ec70435c0cdfadc1fa3ff127191da28eb
Shoham Katzav <shohamkatzav95@gmail.com>
2023-10-16 17:46:41 +0300

    vercel.json

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     server: {
       port: 3000,
       proxy: {
-        "/api/v1": "https://react-express-crud-eight.vercel.app/",
+        "/api/v1": "https://react-express-crud-eight.vercel.app",
       },
     },
     plugins: [react()],
