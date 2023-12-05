e8741bdadc5d6f6e757d9ee1e32b4954930c36ab
Shoham Katzav <shohamkatzav95@gmail.com>
2023-12-05 14:00:06 +0200

    Mobile UI tweaks

--- client/src/components/addTodoForm.jsx
+++ client/src/components/addTodoForm.jsx
@@ -29,10 +29,14 @@ function AddToDoForm({ addTodo }) {
         <form onSubmit={handleAddTodo}>
           <h3>Add a new task</h3>
           <div className="small-margin-top">
-            <label> Task: </label>
-            <input type="text" ref={newTodoInput} />
-            <label> Status: </label>
-            <input type="checkbox" ref={newTodoCheckbox} />
+            <span className="new-todo-parameter">
+              <label> Task: </label>
+              <input type="text" ref={newTodoInput} />
+            </span>
+            <span className="new-todo-parameter">
+              <label> Status: </label>
+              <input type="checkbox" ref={newTodoCheckbox} />
+            </span>
           </div>
           <div className="small-margin-top">
             <button type="submit" className="small-margin-top">Add Todo</button>
